368156a38c8ea583e86748fc90a94becedceead3
gyuchanlee <hoo788@gmail.com>
2025-03-06 15:31:43 +0900

    2025-03-06 docker compose 설정 6

--- Dockerfile
+++ Dockerfile
@@ -9,9 +9,7 @@ RUN npm ci
 
 COPY . ./
 
-ENTRYPOINT ["npm", "run", "start"]
-
 # 중요: Cloud Run에서 사용할 포트 노출
 EXPOSE 8080
 
-CMD ["pnpm", "run", "preview", "--", "--host", "0.0.0.0", "--port", "8080"]
+ENTRYPOINT ["npm", "run", "start"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BldgModule } from './bldg/bldg.module';
+import * as process from 'node:process';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.CLOUDSQL_HOST,
-      // port: parseInt(process.env.CLOUDSQL_PORT),
+      host:
+        process.env.CLOUDSQL_HOST ||
+        `/cloudsql/${process.env.CLOUDSQL_INSTANCE_CONNECTION_NAME}`,
       port: 5432,
       username: process.env.CLOUDSQL_USER,
       password: process.env.CLOUDSQL_PASS,
